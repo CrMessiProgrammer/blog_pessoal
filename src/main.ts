13bbc57cef540c8ec51a1054c73f60a723f2665a
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-01-27 23:41:01 -0300

    :sparkles: Atualização do Deploy - Blog Pessoal

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
   .setTitle('Blog Pessoal')
   .setDescription('Projeto Blog Pessoal')
-  .setContact("Generation Brasil","http://www.generationbrasil.online","generation@email.com")
+  .setContact("Carlos Henrique Nunes","https://github.com/CrMessiProgrammer/blog_pessoal/tree/main","carloshnunes383@gmail.com")
   .setVersion('1.0')
   .addBearerAuth()
   .build();
